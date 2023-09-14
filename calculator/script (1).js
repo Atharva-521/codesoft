51d5be88319c5ac1c1c9cf0b9d53378c8b4252aa
Atharva Kamble <collageandwork@gmail.com>
2023-09-14 20:44:25 +0530

    Added alerts for invalid choices

--- calculator/script (1).js
+++ calculator/script (1).js
@@ -3,25 +3,44 @@ let buttons = document.querySelectorAll('button');
 
 let string = "";
 let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
 
-        else if(e.target.innerHTML == 'AC'){
+function isInvalidExpression(expression) {
+    try {
+        // Check for division by zero
+        if (expression.includes('/ 0')) {
+            return true;
+        }
+        
+        // Try evaluating the expression without actually updating the result
+        eval(expression);
+        
+        // Additional checks for invalid operations can be added here
+        
+        return false;
+    } catch (error) {
+        // An error occurred, so the expression is invalid
+        return true;
+    }
+}
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if (e.target.innerHTML == '=') {
+            if (isInvalidExpression(string)) {
+                alert("Invalid operation");
+            } else {
+                string = eval(string);
+                input.value = string;
+            }
+        } else if (e.target.innerHTML == 'AC') {
             string = "";
             input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length-1);
+        } else if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
             input.value = string;
-        }
-        else{
+        } else {
             string += e.target.innerHTML;
             input.value = string;
         }
-        
-    })
-})
+    });
+});
